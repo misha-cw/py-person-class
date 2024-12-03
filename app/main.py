e7e0b769d8778dd88194165247812e64a3c0a210
Misha Bigun <bigun.mykhaylo@gmail.com>
2024-12-03 21:20:15 +0200

    fix of the specified errors

--- app/main.py
+++ app/main.py
@@ -4,16 +4,15 @@ class Person:
     def __init__(self, name: str, age: int, ) -> None:
         self.name = name
         self.age = age
-        self.people[name] = self
+        Person.people[name] = self
 
 
 def create_person_list(people: list) -> list:
     result = [Person(person["name"], person["age"]) for person in people]
     for i in range(len(people)):
         for person in result:
-            if "husband" in people[i] and people[i]["husband"] == person.name:
+            if people[i].get("husband") and people[i]["husband"] == person.name:
                 result[i].husband = person
-            if "wife" in people[i] and people[i]["wife"] == person.name:
+            if people[i].get("wife") and people[i]["wife"] == person.name:
                 result[i].wife = person
-
     return result
