2b73ba30f556833acb7ca4a58cbd9f6b7443c08e
Misha Bigun <bigun.mykhaylo@gmail.com>
2024-12-03 21:22:00 +0200

    fix of the specified errors

--- app/main.py
+++ app/main.py
@@ -11,7 +11,8 @@ def create_person_list(people: list) -> list:
     result = [Person(person["name"], person["age"]) for person in people]
     for i in range(len(people)):
         for person in result:
-            if people[i].get("husband") and people[i]["husband"] == person.name:
+            if (people[i].get("husband")
+                    and people[i]["husband"] == person.name):
                 result[i].husband = person
             if people[i].get("wife") and people[i]["wife"] == person.name:
                 result[i].wife = person
